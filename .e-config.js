000f7b90f561b6807b02cdb5874ef5010011a750
duanguang <duanguang@hoolinks.com>
2020-08-26 15:23:30 +0800

    fix: 修正版本lock文件

--- .e-config.js
+++ .e-config.js
@@ -1,30 +1,28 @@
-var packageConfig=require('./package.json');
-var proxyConfig=require('./src/home/constants/proxyConfig')
+var packageConfig = require('./package.json');
+var proxyConfig = require('./src/home/constants/proxyConfig')
 module.exports = function (configs) {
-    configs = Object.assign({}, configs, {
-        name: packageConfig.name,
-        defaultPort: 8006,
-        disableEslint:true,
-        chunkhash:true,
-        devServer: Object.assign({},
-            configs.devServer,
-            {
-                 proxy:proxyConfig
-            }),
-        apps: ['home'],
-        webpack: {
-            dllConfig: {
-                vendors: ['vue/dist/vue.esm.js','vue-router','vuex','vue-class-component']
-            },
-            happypack:true
-        },
-        htmlWebpackPlugin: {
-            title: "SCP",
-        },
-        babel:{
-            include:[]
-        }
-    });
-    return configs;
+  configs = Object.assign({}, configs, {
+    name: packageConfig.name,
+    defaultPort: 8006,
+    disableEslint: true,
+    chunkhash: true,
+    devServer: Object.assign({},
+      configs.devServer, {
+        proxy: proxyConfig
+      }),
+    apps: ['home'],
+    webpack: {
+      dllConfig: {
+        vendors: ['vue/dist/vue.esm.js', 'vue-router', 'vuex', 'vue-class-component']
+      },
+      happypack: true
+    },
+    htmlWebpackPlugin: {
+      title: "webapp",
+    },
+    babel: {
+      include: []
+    }
+  });
+  return configs;
 };
-
